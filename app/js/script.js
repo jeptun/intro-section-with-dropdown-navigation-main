28c212df447ced898142d9bfce90169acb4efa58
jeptun <jeptun@gmail.com>
2022-08-16 21:19:55 +0200

    Changes

--- app/js/script.js
+++ app/js/script.js
@@ -24,3 +24,20 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", false);
   }
 });
+
+//Dropdown Menu
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
